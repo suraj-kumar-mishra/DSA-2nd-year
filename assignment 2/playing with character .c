59ae56e6047e516411166dc2bc31345da47616fa
suraj-kumar-mishra <114098189+suraj-kumar-mishra@users.noreply.github.com>
2022-09-25 22:07:20 +0530

    Create playing with character .c

---
+++ assignment 2/playing with character .c
@@ -0,0 +1,19 @@
+https://www.hackerrank.com/challenges/playing-with-characters/problem?isFullScreen=true
+
+
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+char ch;
+char s[20],sen[100];
+//string s,sen;
+scanf("%c",&ch);
+scanf("%s",s);
+scanf("\n");
+gets(sen);
+printf("%c \n",ch);
+printf("%s \n",s);
+puts(sen);
+    return 0;
+}
